dab87516232fe0e6edd10e3d3735d04551acc3f8
rrassya19-bit <rrassys19@gmail.com>
2026-07-20 10:57:43 +0700

    Add initial server setup with Express and Sequelize ORM

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const db = require("./models");
+
+app.use(express.json());
+app.use(express.urlencoded({ 
+    extended: false
+}));
+
+app.listen(port, () => {
+    console.log(`Server started on port 3000`);
+})
+
+db.sequelize.sync()
+    .then(() => {
+        app.listen(3000, () => {
+            console.log("Server Started");
+        })
+    })
+
+    .catch((err) => {
+        console.log(err);
+    });
